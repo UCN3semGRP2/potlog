d6dc712cd39fbfa55dfa50de07f3eb2a8cb64f46
Anders <a.jensen1996@gmail.com>
2017-11-10 11:29:40 +0100

    Created ValidatePassword() in the UserCtrl.

--- Backend/Backend/BLL/UserCtrl.cs
+++ Backend/Backend/BLL/UserCtrl.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace BLL
 {
     public class UserCtrl
     {
+        public bool ValidatePassword(User u, string clearTextPw)
+        {
+            return HashingHelper.CheckPassword(clearTextPw, u.Salt, u.Password); 
+             
+        }
     }
 }
