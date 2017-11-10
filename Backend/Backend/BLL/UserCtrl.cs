5fd598efd5f672b69707b4b886034c7429d8bfc2
Rasmus Nedergaard Pedersen <rnp@rasmusnedergaard.dk>
2017-11-10 11:30:13 +0100

    CreateUser method added to UserCtrl in BLL in Backend

--- Backend/Backend/BLL/UserCtrl.cs
+++ Backend/Backend/BLL/UserCtrl.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
+using DAL;
 
 namespace BLL
 {
     public class UserCtrl
     {
+        public void CreateUser(string Firstname, string Lastname, string Email, string Password)
+        {
+
+            string salt = HashingHelper.GenerateSalt();
+            string hashedPassword = HashingHelper.HashPassword(Password, salt);
+
+            User user = new User
+            {
+                Firstname = Firstname,
+                Lastname = Lastname,
+                Email = Email,
+                Password = hashedPassword,
+                Salt = salt
+            };
+
+            using (DALContext db = new DALContext())
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+        }
     }
 }
