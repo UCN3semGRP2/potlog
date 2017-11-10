574db49d9d08fd347bad9c43adb39b90b8d51519
Rasmus Nedergaard Pedersen <rnp@rasmusnedergaard.dk>
2017-11-10 11:00:33 +0100

    Salt added to User model in backend

--- Backend/Backend/Model/User.cs
+++ Backend/Backend/Model/User.cs
@@ -13,5 +13,6 @@ namespace Model
         public string Lastname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string Salt { get; set; }
     }
 }
