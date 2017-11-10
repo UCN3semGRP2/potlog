351a4769d905e79e0ea12e62e2dacb8665fdf053
Rasmus Nedergaard Pedersen <rnp@rasmusnedergaard.dk>
2017-11-10 10:22:05 +0100

    Fixed connection string name

--- Backend/Backend/DAL/DALContext.cs
+++ Backend/Backend/DAL/DALContext.cs
@@ -8,7 +8,7 @@ namespace DAL
     public class DALContext : DbContext
     {
         public DALContext()
-            : base("name=DALContext.cs")
+            : base("name=DALContext")
         {
 
         }
