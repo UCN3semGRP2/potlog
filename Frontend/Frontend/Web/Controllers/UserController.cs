d08d03182f4e5b8384049970b1c15afa38f9ba12
Rasmus Nedergaard Pedersen <rnp@rasmusnedergaard.dk>
2017-11-14 11:30:02 +0100

    Create User in Web applikation created.

---
+++ Frontend/Frontend/Web/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class UserController : Controller
+    {
+        ServiceReference.IService service = new ServiceReference.ServiceClient();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            service.CreateUser(model.Firstname, model.Lastname, model.Email, model.Password);
+
+            //TODO
+            return RedirectToAction("TODO");
+        }
+    }
+}
---
+++ Frontend/Frontend/Web/Models/CreateViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class CreateViewModel
+    {
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "Email skal udfyldes.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Display(Name = "Fornavn")]
+        [Required(ErrorMessage = "Fornavn skal udfyldes.")]
+        public string Firstname { get; set; }
+        [Display(Name = "Efternavn")]
+        [Required(ErrorMessage = "Efternavn skal udfyldes.")]
+        public string Lastname { get; set; }
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password skal udfyldes.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Gentag Password")]
+        [Required(ErrorMessage = "Password og Gentag Password skal være identiske.")]
+        [DataType(DataType.Password)]
+        public string RepeatPassword { get; set; }
+    }
+}
