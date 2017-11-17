4b2c7c2a8fbec29ce3d8adeeceb02c9cb2f52004
Niklas Jørgensen <niklas220587@hotmail.com>
2017-11-17 13:35:52 +0100

    Finished test first for create registration

--- Backend/Backend/BLL/RegistrationCtrl.cs
+++ Backend/Backend/BLL/RegistrationCtrl.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
 
 namespace BLL
 {
     public class RegistrationCtrl
     {
+        public Registration CreateRegistration(User user, Event eve)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Backend/Backend/BLLTest/RegistrationCtrlTest.cs
+++ Backend/Backend/BLLTest/RegistrationCtrlTest.cs
@@ -15,8 +15,21 @@ namespace BLLTest
         [TestMethod]
         public void CreateRegistrationTest()
         {
+            // Arrange
             UserCtrl uCtrl = new UserCtrl();
-            uCtrl.CreateUser("Jesper", "Jørgensen", "e@w.dk", "1234");
+            var user = uCtrl.CreateUser("Jesper", "Jørgensen", "e@w.dk", "1234");
+            EventCtrl eCtrl = new EventCtrl();
+            var eve = eCtrl.CreateEvent("Hej", "nej", 5, 5.5, 6.5, "42", DateTime.Now, false);
+            RegistrationCtrl rCtrl = new RegistrationCtrl();
+
+            // Act
+            var reg = rCtrl.CreateRegistration(user, eve);
+
+            //Assert
+            bool userHasReg = user.Registrations.Contains(reg);
+            bool eventHasReg = eve.Registrations.Contains(reg);
+            Assert.IsTrue(userHasReg);
+            Assert.IsTrue(eventHasReg);
         }
     }
 }
