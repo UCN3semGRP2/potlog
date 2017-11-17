25f1b1bb0cafd8e82c2818717a34a1c35946837d
Niklas Jørgensen <niklas220587@hotmail.com>
2017-11-17 09:02:52 +0100

    Created model for Event

---
+++ Backend/Backend/Model/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int NumOfParticipants { get; set; }
+        public double PriceFrom { get; set; }
+        public double PriceTo { get; set; }
+        public string Location { get; set; }
+        public DateTime datetime { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
