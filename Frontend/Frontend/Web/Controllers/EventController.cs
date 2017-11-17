424a1e5788e81763cdb6bf03a47b81c3a27a8999
Niklas Jørgensen <niklas220587@hotmail.com>
2017-11-17 11:23:51 +0100

    Implemented CreateEvent controller logic

--- Frontend/Frontend/Web/Controllers/EventController.cs
+++ Frontend/Frontend/Web/Controllers/EventController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Models;
 
 namespace Web.Controllers
 {
     public class EventController : Controller
     {
+        ServiceReference.IService service = new ServiceReference.ServiceClient();
+
         // GET: Event
         public ActionResult Index()
         {
@@ -16,6 +19,27 @@ namespace Web.Controllers
 
         [HttpGet]
         public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateEventViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            DateTime dt = model.Date + model.Time;
+
+            var evnt = service.CreateEvent(model.Title, model.Description, model.NumOfParticipants, model.PriceFrom, model.PriceTo, model.Location, dt, model.IsPublic);
+
+            return RedirectToAction("CreateSuccess");
+        }
+
+        [HttpGet]
+        public ActionResult CreateSuccess()
         {
             return View();
         }
