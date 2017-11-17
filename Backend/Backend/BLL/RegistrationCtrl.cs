d0f9313870a81c278e9c046bcb30b11df52760b9
Niklas Jørgensen <niklas220587@hotmail.com>
2017-11-17 13:46:23 +0100

    Created stub method for createdregistration in registrationCtrl

--- Backend/Backend/BLL/RegistrationCtrl.cs
+++ Backend/Backend/BLL/RegistrationCtrl.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model;
+using DAL;
 
 namespace BLL
 {
     public class RegistrationCtrl
     {
+        //EventCtrl eCtrl = new EventCtrl();
+        //UserCtrl uCtrl = new UserCtrl();
+        //RegistrationDB rDB = new RegistrationDB();
         public Registration CreateRegistration(User user, Event eve)
         {
+            //Registration reg = new Registration
+            //{
+            //    DateOfCreation = DateTime.Now
+            //};
+            //reg = rDB.Create(reg);
+            //eCtrl.AddRegistration(reg, eve);
+            //uCtrl.AddRegistration(reg, user);
+            //return reg;
             throw new NotImplementedException();
         }
     }
