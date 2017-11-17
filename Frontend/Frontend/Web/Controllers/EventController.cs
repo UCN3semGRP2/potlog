5a8e615807dcf2314935529cf10d0140fb7f8133
Niklas Jørgensen <niklas220587@hotmail.com>
2017-11-17 10:31:17 +0100

    Created controller and ViewModel for create event

---
+++ Frontend/Frontend/Web/Controllers/EventController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/Frontend/Web/Models/CreateEventViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class CreateEventViewModel
+    {
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public int NumOfParticipants { get; set; }
+        public bool IsPublic { get; set; }
+        public string Description { get; set; }
+        public double PriceFrom { get; set; }
+        public double PriceTo { get; set; }
+    }
+}
