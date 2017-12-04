4dce951aa7e9f7d5428df9eb66634cec3518a4b6
Anders <a.jensen1996@gmail.com>
2017-12-04 13:23:33 +0100

    Wrote service tests for SignUpForEvent

---
+++ Backend/Backend/PotLogServiceTests/CreateEventTests.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotLogServiceTests.ServiceReference;
+using System.ServiceModel;
+
+namespace PotLogServiceTests
+{
+    [TestClass]
+    public class CreateEventTests
+    {
+        private ServiceReference.IService service = new ServiceReference.ServiceClient();
+
+        public User User { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var email = "t@t.t" + Guid.NewGuid();
+            var pw = "hunter1";
+            service.CreateUser("TestCreateEventUser", "Test", email, pw);
+            this.User = service.LogIn(email, pw);
+        }
+
+        [TestMethod]
+        public void TestCreateEventHappyDay()
+        {
+            string title = "title";
+            string description = "description";
+            int numOfParticipants = 5;
+            double priceFrom = 5;
+            double priceTo = 10;
+            string location = "her";
+            DateTime datetime = DateTime.Now.AddDays(5);
+            bool isPublic = false;
+            User admin = User;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.AreEqual(title, evnt.Title);
+            Assert.AreEqual(description, evnt.Description);
+            Assert.AreEqual(numOfParticipants, evnt.NumOfParticipants);
+            Assert.AreEqual(priceFrom, evnt.PriceFrom);
+            Assert.AreEqual(priceTo, evnt.PriceTo);
+            Assert.AreEqual(location, evnt.Location);
+            Assert.AreEqual(datetime, evnt.Datetime);
+            Assert.AreEqual(isPublic, evnt.IsPublic);
+            Assert.AreEqual(admin.Id, evnt.Admin.Id);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void TestCreateEventNullAdmin()
+        {
+            string title = "title";
+            string description = "description";
+            int numOfParticipants = 5;
+            double priceFrom = 5;
+            double priceTo = 10;
+            string location = "her";
+            DateTime datetime = DateTime.Now.AddDays(5);
+            bool isPublic = false;
+            User admin = null;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void TestCreateEventWithNoTitle()
+        {
+            string title = "";
+            string description = "description";
+            int numOfParticipants = 5;
+            double priceFrom = 5;
+            double priceTo = 10;
+            string location = "her";
+            DateTime datetime = DateTime.Now.AddDays(5);
+            bool isPublic = false;
+            User admin = User;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void TestCreateEventWithNoDescription()
+        {
+            string title = "title";
+            string description = "";
+            int numOfParticipants = 5;
+            double priceFrom = 5;
+            double priceTo = 10;
+            string location = "her";
+            DateTime datetime = DateTime.Now.AddDays(5);
+            bool isPublic = false;
+            User admin = User;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void TestCreateEventWithNegativeParticipants()
+        {
+            string title = "title";
+            string description = "description";
+            int numOfParticipants = -5;
+            double priceFrom = 5;
+            double priceTo = 10;
+            string location = "her";
+            DateTime datetime = DateTime.Now.AddDays(5);
+            bool isPublic = false;
+            User admin = User;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void TestCreateEventWithZeroParticipants()
+        {
+            string title = "title";
+            string description = "description";
+            int numOfParticipants = 0;
+            double priceFrom = 5;
+            double priceTo = 10;
+            string location = "her";
+            DateTime datetime = DateTime.Now.AddDays(5);
+            bool isPublic = false;
+            User admin = User;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void TestCreateEventWithNegativePriceFrom()
+        {
+            string title = "title";
+            string description = "description";
+            int numOfParticipants = 5;
+            double priceFrom = -5;
+            double priceTo = 10;
+            string location = "her";
+            DateTime datetime = DateTime.Now.AddDays(5);
+            bool isPublic = false;
+            User admin = User;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void TestCreateEventWithNegativePriceTo()
+        {
+            string title = "title";
+            string description = "description";
+            int numOfParticipants = 5;
+            double priceFrom = -50;
+            double priceTo = -10;
+            string location = "her";
+            DateTime datetime = DateTime.Now.AddDays(5);
+            bool isPublic = false;
+            User admin = User;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void TestCreateEventWithPriceToLesThanPriceFrom()
+        {
+            string title = "title";
+            string description = "description";
+            int numOfParticipants = 5;
+            double priceFrom = 10;
+            double priceTo = 5;
+            string location = "her";
+            DateTime datetime = DateTime.Now.AddDays(5);
+            bool isPublic = false;
+            User admin = User;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void TestCreateEventWithDateInThePast()
+        {
+            string title = "title";
+            string description = "description";
+            int numOfParticipants = 5;
+            double priceFrom = 5;
+            double priceTo = 10;
+            string location = "her";
+            DateTime datetime = new DateTime(1990,3,3);
+            bool isPublic = false;
+            User admin = User;
+
+            var evnt = service.CreateEvent(title, description, numOfParticipants, priceFrom, priceTo, location, datetime, isPublic, admin);
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
---
+++ Backend/Backend/PotLogServiceTests/FindEventByIdTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotLogServiceTests.ServiceReference;
+using System.Linq;
+
+namespace PotLogServiceTests
+{
+    [TestClass]
+    public class FindEventByIdTests
+    {
+        private ServiceReference.IService service = new ServiceReference.ServiceClient();
+
+        public User User { get; set; }
+        public Event Evnt { get; set; }
+        public int EventId { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var email = "t@t.t" + Guid.NewGuid();
+            var pw = "hunter1";
+            service.CreateUser("TestCreateEventUser", "Test", email, pw);
+            this.User = service.LogIn(email, pw);
+
+            this.Evnt = service.CreateEvent("test event", "test event please ignore", 5, 10.0, 100.5, "here", DateTime.Now.AddDays(5), false, this.User);
+            this.EventId = Evnt.Id;
+        }
+
+        [TestMethod]
+        public void TestFindEventByIdHappyDay()
+        {
+            var e = service.FindEventById(EventId);
+
+            Assert.AreEqual(Evnt.Id, e.Id);
+            Assert.AreEqual(Evnt.Title, e.Title);
+            Assert.AreEqual(Evnt.Description, e.Description);
+            Assert.AreEqual(Evnt.Location, e.Location);
+            Assert.AreEqual(Evnt.Datetime, e.Datetime);
+            Assert.AreEqual(Evnt.IsPublic, e.IsPublic);
+            Assert.AreEqual(Evnt.NumOfParticipants, e.NumOfParticipants);
+            Assert.AreEqual(Evnt.PriceFrom, e.PriceFrom);
+            Assert.AreEqual(Evnt.PriceTo, e.PriceTo);
+        }
+
+        [TestMethod]
+        public void TestFindEvent()
+        {
+            var e = service.FindEventById(EventId);
+            var regs = e.Registrations;
+
+            bool UserIsAdmin = regs
+                .Select(reg => reg.Event)
+                .Where(ev => ev.Admin.Id == User.Id)
+                .Select(x => x.Admin.Id)
+                .Contains(User.Id);
+            Assert.IsTrue(UserIsAdmin, "User is admin");
+
+            foreach (var reg in regs)
+            {
+                Assert.AreEqual(Evnt.Id, reg.Event.Id);
+            }
+
+            
+        }
+    }
+}
