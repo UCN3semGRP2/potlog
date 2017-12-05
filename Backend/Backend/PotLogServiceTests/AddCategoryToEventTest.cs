2f0ab9804c9b85586d57f5055756d013481be2aa
Anders <a.jensen1996@gmail.com>
2017-12-05 09:25:49 +0100

    Finished AddCategoryToEventTests for the service tests

--- Backend/Backend/PotLogServiceTests/AddCategoryToEventTest.cs
+++ Backend/Backend/PotLogServiceTests/AddCategoryToEventTest.cs
@@ -11,23 +11,59 @@ namespace PotLogServiceTests
         private ServiceReference.IService service = new ServiceReference.ServiceClient();
 
         public User User { get; set; }
-        public Event Evnt { get; set; }
-        public int EventId { get; set; }
-        public Component Cat { get; set; }
 
         [TestInitialize]
         public void TestInitialize()
         {
-            this.Evnt = service.CreateEvent("test event", "test event please ignore", 5, 10.0, 100.5, "here", DateTime.Now.AddDays(5), false, this.User);
-            this.EventId = Evnt.Id;
+            var email = "t@t.t" + Guid.NewGuid();
+            var pw = "hunter1";
+            service.CreateUser("TestCreateEventUser", "Test", email, pw);
+            this.User = service.LogIn(email, pw);
+
+            
         }
 
         [TestMethod]
         public void TestAddCategoryToEventHappyDays()
         {
-            service.AddCategoryToEvent(Evnt.Id, "Cat Title", "Cat Description");
-            Category c = Evnt.Components.Where(x => x.Title == "Cat Title" && x is Category).FirstOrDefault() as Category;
-            Assert.IsTrue(c.EventId == Evnt.Id);
+            var evnt = service.CreateEvent("test event", "test event please ignore", 5, 10.0, 100.5, "here", DateTime.Now.AddDays(5), false, this.User);
+
+            var title = "Cat Title";
+            var description = "Cat Description";
+            service.AddCategoryToEvent(evnt.Id, title, description);
+
+            var foundEvnt = service.FindEventById(evnt.Id);
+
+            Assert.IsNotNull(foundEvnt.Components);
+            Assert.AreEqual(foundEvnt.Components.Length, 1);
+            var cat = foundEvnt.Components[0];
+            Assert.IsTrue(cat is Category);
+            Assert.AreEqual(title, cat.Title);
+            Assert.AreEqual(description, cat.Description);
+        }
+
+        [TestMethod]
+        public void TestAddCategoryToEventNoTitle()
+        {
+            var title = "";
+            var description = "Description with no title";
+            var evnt = service.CreateEvent("test event", "test event please ignore", 5, 10.0, 100.5, "here", DateTime.Now.AddDays(5), false, this.User);
+
+            service.AddCategoryToEvent(evnt.Id, title, description);
+            
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestAddCategoryToEventNoDescription()
+        {
+            var title = "Title with no description";
+            var description = "";
+            var evnt = service.CreateEvent("test event", "test event please ignore", 5, 10.0, 100.5, "here", DateTime.Now.AddDays(5), false, this.User);
+
+            service.AddCategoryToEvent(evnt.Id, title, description);
+   
+            Assert.Fail();
         }
     }
 }
