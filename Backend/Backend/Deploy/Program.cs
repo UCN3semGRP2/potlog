36cf4b7947d4ef38cef7561df4e56546799b7833
Anders <a.jensen1996@gmail.com>
2017-12-13 10:49:25 +0100

    Set up the basic service host.

---
+++ Backend/Backend/Deploy/Program.cs
@@ -0,0 +1,23 @@
+﻿using PotLogService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+namespace SecureService.Hosts.ConsoleHost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //This will host both wsHttp and netTcp bindings of secureservice
+            ServiceHost host = new ServiceHost(typeof(Service));
+            host.Open();
+            Console.WriteLine("SecureService host is running...");
+            Console.ReadLine();
+            host.Close();
+
+        }
+    }
+}
